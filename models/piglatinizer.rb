4f17f460e61c23a5f1ae6976f5ef8db11919265a
Brandon Leeds <brandonleeds@gmail.com>
2018-02-09 21:02:12 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -4,10 +4,10 @@ class PigLatinizer
 
   def piglatinize word
     if word =~ (/\A[aeiou]/i)
-      word = word + 'ay'
+      word = word + 'way'
     elsif word =~ (/\A[^aeiou]/i)
       match = /\A[^aeiou]/i.match(word)
-      word = match.post_match + match.to_s + 'way'
+      word = match.post_match + match.to_s + 'ay'
     end
     word
   end
