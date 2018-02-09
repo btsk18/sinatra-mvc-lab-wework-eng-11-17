60c56d1c297af46dc5724c831947a55aed2700e7
Brandon Leeds <brandonleeds@gmail.com>
2018-02-09 21:05:49 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,12 +1,9 @@
 class PigLatinizer
-
-
-
   def piglatinize sentence
     sentence.split(" ").map do |word|
       word = word.gsub("qu", " ")
       word.gsub!(/^([^aeiou]*)(.*)/,'\2\1ay')
       word = word.gsub(" ", "qu")
-    end
+    end.first
   end
 end
