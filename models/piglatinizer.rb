616cc8486001e613f0ee0cdb0a55cbc57764eccb
Brandon Leeds <brandonleeds@gmail.com>
2018-02-09 20:59:17 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,9 +2,21 @@ class PigLatinizer
 
 
 
-  def piglatinize(word)
-    word.reverse
+  def piglatinize str
+  alpha = ('a'..'z').to_a
+  vowels = %w[a e i o u]
+  consonants = alpha - vowels
+
+  if vowels.include?(str[0])
+    str + 'ay'
+  elsif consonants.include?(str[0]) && consonants.include?(str[1])
+    str[2..-1] + str[0..1] + 'ay'
+  elsif consonants.include?(str[0])
+    str[1..-1] + str[0] + 'ay'
+  else
+    str # return unchanged
   end
+end
 
   def to_pig_latin(words)
     words.split(" ").reverse.map { |t| t.reverse }.join(" ")
